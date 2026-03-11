75067207b2e79542f82fcafd52fddd6ac32042b0
krishnaveni <krishnavenivadla55@gmail.com>
2026-03-11 21:58:33 +0530

    mes

--- IV - SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV - SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,17 +1,14 @@
 def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    avg = (n1 + n2 + n3) / 3
-
-    if avg >= 35:
+    average = (n1 + n2 + n3) / 3
+    average = int(average*100)/100
+    if average >= 35:
         status = "Pass"
     else:
         status = "fail"
 
-    avg = round(avg, 2)
-
-    return f"Average grade: {avg}, Status: {status}"
-
+    return f"Average grade: {average}, Status: {status}"
 
 if __name__ == '__main__':
     name = input()
-    n1, n2, n3 = list(map(int, input().split()))
-    print(Student_Grade_System(name, n1, n2, n3))
+    n1,n2,n3 = list(map(int,input().split()))
+    print(Student_Grade_System(name,n1,n2,n3))
